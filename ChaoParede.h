47edfd58fce3fc32e04bb066dd99d68326e92a20
VinCouto <166947709+VinCouto@users.noreply.github.com>
2025-11-24 18:43:45 -0300

    Add files via upload

--- ChaoParede.h
+++ ChaoParede.h
@@ -10,6 +10,8 @@ typedef struct{
 } wall;
 
 wall* wall_create(unsigned short width, unsigned short height, unsigned short pos_x, unsigned short pos_y);
+void wall_draw(wall* element, float camera_x, float camera_y);
+
 int check_collision_wall(square *player, wall *w);
 void wall_destroy(wall* element);
 
